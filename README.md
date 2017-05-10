eaac11863d3d8165d7edc4a26684d9ca96aa34c2
zigomir <zigomir@gmail.com>
2017-05-10 11:31:55 -0700

    First!

---
+++ README.md
@@ -0,0 +1,18 @@
+# Typed Vue
+
+> Vue, Typescript and Webpack
+
+## Features
+
+- type checking inside `.vue` files with `lang="ts"` attribute (holy grail!)
+- hot reload with `webpack`
+- no babel
+
+## Conventions / opinions
+
+- no source maps in development for smaller loads
+- no vendor splitting and adding hashes to file names at this point, can be easily added
+
+## Thanks
+
+All ripped off from [Microsoft/TypeScript-Vue-Starter](https://github.com/Microsoft/TypeScript-Vue-Starter)
---
+++ template/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            ts: 'ts-loader',
+            file: 'file-loader'
+          }
+        }
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = [
+    new webpack.DefinePlugin({ 
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]
+}
