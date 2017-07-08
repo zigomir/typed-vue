064167bf2ca5d808b1fae9e0bfe2adaec8b95b4a
zigomir <zigomir@gmail.com>
2017-07-08 09:52:05 -0700

    Moar shims, more typing.

--- template/webpack-shims.d.ts
+++ template/webpack-shims.d.ts
@@ -1,4 +1,21 @@
-// TODO: declare plugins here
-// HtmlWebpackPlugin
-// ExtractTextPlugin
-declare module '*'
+declare module 'html-webpack-plugin' {
+  import { Plugin } from 'webpack'
+
+  class HtmlWebpackPlugin extends Plugin {
+    constructor(options: {[key: string]: any})
+  }
+
+  module HtmlWebpackPlugin {} // https://github.com/Microsoft/TypeScript/issues/6656
+  export = HtmlWebpackPlugin
+}
+
+declare module 'extract-text-webpack-plugin' {
+  import { Plugin } from 'webpack'
+
+  class ExtractTextPlugin extends Plugin {
+    constructor(name: string)
+  }
+
+  module ExtractTextPlugin {}
+  export = ExtractTextPlugin
+}
